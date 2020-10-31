0caf68005305364f8a8fdaf921c649d50d49bd9d
ariefzuhri <ariefzuhri72@gmail.com>
2020-10-31 09:29:24 +0700

    Tambah aset

---
+++ app/src/main/java/com/ppab1/dreamsaver/TestingActivity.java
@@ -0,0 +1,14 @@
+package com.ppab1.dreamsaver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testing);
+    }
+}
