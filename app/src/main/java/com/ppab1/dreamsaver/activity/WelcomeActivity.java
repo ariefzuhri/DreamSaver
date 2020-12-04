c35ce903c72a6e0f92e3bb4078acbdc376f2a3c8
amindraa05 <amindraa05@gmail.com>
2020-12-04 10:44:28 +0700

    WelcomeActivity xml bg blue

---
+++ app/src/main/java/com/ppab1/dreamsaver/activity/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.ppab1.dreamsaver.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ppab1.dreamsaver.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
