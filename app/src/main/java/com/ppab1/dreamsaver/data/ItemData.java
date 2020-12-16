a4c5688bcb8753e9a159e7a0555d5a388fd2f782
muri27 <muri.rifki27@gmail.com>
2020-12-16 13:44:14 +0700

    Tambah Kelola Rencana

---
+++ app/src/main/java/com/ppab1/dreamsaver/data/ItemData.java
@@ -0,0 +1,8 @@
+package com.ppab1.dreamsaver.data;
+
+public class ItemData {
+    public String savingTarget;
+    public String dateTarget;
+    public String dailyTarget;
+    public String totalTarget;
+}
